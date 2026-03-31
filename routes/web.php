408edb6b142a5c0f6074d25dd7c2648b93012749
Phạm Tuấn Anh <tuananhtybg.99@gmail.com>
2026-03-31 11:02:09 +0700

    clean code

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    // Todo: Sau này cần sửa lại không trỏ trực tiếp vào login mà phải trả về một trang welcome để giới thiệu về ứng dụng, sau đó mới có nút đăng nhập
     return redirect()->route('login');
 });
 
